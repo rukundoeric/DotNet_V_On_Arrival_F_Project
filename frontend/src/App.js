b1de6c407d2df885f1ce007454bf2ef9876b1dcf
Rukundo Eric <ericrukundo005@gmail.com>
2025-12-13 15:47:54 +0200

    fix some issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
